4d300bc363c3c14f21fd5303b8fe1c7fa3db8c95
Aymen Maachi <kraymis.maachi@gmail.com>
2024-08-19 19:22:46 +0100

    1.25

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: 'http://localhost:5173', // Replace with your frontend's URL
+        origin: 'https://jwtapp-fg84.onrender.com', // Replace with your frontend's URL
         methods: ['GET', 'POST'],
         allowedHeaders: ['Authorization'],
         credentials: true,
