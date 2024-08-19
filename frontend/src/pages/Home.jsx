1812c3b834d61b81c1ceb7841e87c44f05181f38
Aymen Maachi <kraymis.maachi@gmail.com>
2024-08-19 19:42:07 +0100

    1.28

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -130,8 +130,8 @@ const Home = () => {
 return (
   <>
   <Navbar onLogout={handleLogout}/>
-  <div className="flex items-start justify-center bg-[#0f0822]">
-    <div className="w-[25vw] ml-4">
+  <div className="flex items-start justify-center bg-[#0f0822] h-screen w-full">
+  <div className="w-[25vw] ml-4">
                 <FriendsList
                   friends={friends}
                   error={error}
